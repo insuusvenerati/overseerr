a3d453e03be398a7add02c71d543ee34a0752992
Ryan Cohen <r-cohen@mercari.com>
2022-08-19 14:16:38 +0900

    test: add watchlist test for discover page

--- cypress/e2e/discover.cy.ts
+++ cypress/e2e/discover.cy.ts
@@ -23,22 +23,65 @@ describe('Discover', () => {
   });
 
   it('loads a trending item', () => {
+    cy.intercept('/api/v1/discover/trending*').as('getTrending');
+    cy.wait('@getTrending');
     clickFirstTitleCardInSlider('Trending');
   });
 
   it('loads popular movies', () => {
+    cy.intercept('/api/v1/discover/movies*').as('getPopularMovies');
+    cy.wait('@getPopularMovies');
     clickFirstTitleCardInSlider('Popular Movies');
   });
 
   it('loads upcoming movies', () => {
+    cy.intercept('/api/v1/discover/movies/upcoming*').as('getUpcomingMovies');
+    cy.wait('@getUpcomingMovies');
     clickFirstTitleCardInSlider('Upcoming Movies');
   });
 
   it('loads popular series', () => {
+    cy.intercept('/api/v1/discover/tv*').as('getPopularTv');
+    cy.wait('@getPopularTv');
     clickFirstTitleCardInSlider('Popular Series');
   });
 
   it('loads upcoming series', () => {
+    cy.intercept('/api/v1/discover/tv/upcoming*').as('getUpcomingSeries');
+    cy.wait('@getUpcomingSeries');
     clickFirstTitleCardInSlider('Upcoming Series');
+  });
+
+  it('loads plex watchlist', () => {
+    cy.intercept('/api/v1/discover/watchlist', { fixture: 'watchlist' }).as(
+      'getWatchlist'
+    );
+
+    cy.wait('@getWatchlist');
+
+    // Wait for one of the watchlist movies to resolve
+    cy.intercept('/api/v1/movie/361743').as('getTmdbMovie');
+
+    const sliderHeader = cy.contains('.slider-header', 'Plex Watchlist');
+
+    sliderHeader.scrollIntoView();
+
+    cy.wait('@getTmdbMovie');
+
+    sliderHeader
+      .next('[data-testid=media-slider]')
+      .find('[data-testid=title-card]')
+      .first()
+      .trigger('mouseover')
+      .find('[data-testid=title-card-title]')
+      .invoke('text')
+      .then((text) => {
+        cy.contains('.slider-header', 'Plex Watchlist')
+          .next('[data-testid=media-slider]')
+          .find('[data-testid=title-card]')
+          .first()
+          .click();
+        cy.get('[data-testid=media-title]').should('contain', text);
+      });
   });
 });
